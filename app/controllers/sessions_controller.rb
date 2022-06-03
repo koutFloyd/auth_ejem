4797a90a2aac229c35777be207d1833fc1be223f
OTORRES <ostorresm@gmail.com>
2022-06-03 02:07:24 -0400

    primer commit

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+    #se pregunta si existe email en el modelo usuario antes de crear el User
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: "Logueado correctamente :D"
+    else
+      redirect_to new_session_path, notice: "No existe cuenta XD"
+    end
   end
 
   def destroy
+    session[:user_id] = nil
+    redirect_to new_session_path, notice: "Adios!... Ha cerrado la sesion :("
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  # se agrega ruta get para borrar una sesion o delete o matar sesion
+  get 'sessions/destroy'
+  
+
   resources :sessions, only: [:new, :create, :destroy]
   root "home#index"
 end
